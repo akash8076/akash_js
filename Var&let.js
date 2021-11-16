69a17b47f64bcf231c57ac1a3cef640ad120355a
akash <akashsojitra44@gmail.com>
2021-11-16 16:34:39 +0530

    first

---
+++ Var&let.js
@@ -0,0 +1,42 @@
+//let x =20;   
+//document.write("Outside the function x = " + x);   
+//show();   
+
+//function show(){   
+//    document.write("<br> Inside the function x = " + x);   
+//} 
+
+// var a=10;
+// {
+    // var a=30;
+    // document.write("x-"+x)
+// }
+// document.write("x-"+x)
+
+// let x = 10;
+// function test()
+// {
+//     let x = 50;
+//     document.write(x);
+// }
+test();
+document.write(x);
+let number =A=>
+{
+    console.log(A)
+}
+(7624078578)
+let no='7624078578'
+ l
+
+
+
+ 
+
+
+
+
+
+
+
+
